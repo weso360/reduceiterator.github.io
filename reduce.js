c447923daf691d899639953b15ff7c203179d14d
WS-code <115361234+weso360@users.noreply.github.com>
2022-12-12 23:44:26 +0000

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,12 @@
+const newNumbers = [1, 3, 5, 7, 10];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) =>{
+
+console.log('The value of accumulator: ', accumulator);
+
+console.log('The value of currentValue: ', currentValue);
+
+return accumulator + currentValue
+},10)
+
+console.log(newSum)
